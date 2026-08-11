c2887a19207aacd437918f8c7f16e2ccd192b0be
editorsteamir-boop <editors.team.ir@gmail.com>
2026-08-11 17:44:50 +0330

    feat: add Fonto library importer script

---
+++ scripts/import_fonto_library.js
@@ -0,0 +1,9 @@
+// Fonto Library Importer
+// Supabase integration placeholder
+
+async function importFontoLibrary() {
+  console.log('Fonto library importer ready');
+  console.log('Next: connect Supabase Storage and database tables');
+}
+
+module.exports = { importFontoLibrary };
